0842403f83ec38d25946512bb06fb4da33cddb21
Sai Sruthi <59644095+Saisruthiamirthaganesh@users.noreply.github.com>
2020-11-30 14:05:49 +0530

    Add files via upload

---
+++ per_chromosome_read_count.py
@@ -0,0 +1,16 @@
+# importing the pysam package that enables the parsing of BAM files to python
+import pysam 
+#built-in method for calling the alignment file
+samfile=pysam.AlignmentFile("<PATH>/filename.sorted")
+# searches for the specified strings in the alignment
+list=("chr1","chr2","chr3","chr4","chr5","chr6","chr7","chr8","chr9","chr10","chr11","chr12","chr13","chr14","chr15","chr16","chr17","chr18","chr19","chr20","chr21”,”chr22”,”chrX")
+# employs the fetch method in a loop to call forth the unique and specific read mapping to a particular chromosome
+for i in list:
+    iter=samfile.fetch(i)
+    print (str(i))
+    count=0
+    for x in iter:
+        print (str(x))
+        count+=1
+        print (count)
+#end
