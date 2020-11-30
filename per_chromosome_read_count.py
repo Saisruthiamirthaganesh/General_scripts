590da052127631194ba00d6fc99d7d0ea6256a2b
Sai Sruthi <59644095+Saisruthiamirthaganesh@users.noreply.github.com>
2020-11-30 14:06:26 +0530

    Update per_chromosome_read_count.py

--- per_chromosome_read_count.py
+++ per_chromosome_read_count.py
@@ -4,7 +4,7 @@ import pysam
 samfile=pysam.AlignmentFile("<PATH>/filename.sorted")
 # searches for the specified strings in the alignment
 list=("chr1","chr2","chr3","chr4","chr5","chr6","chr7","chr8","chr9","chr10","chr11","chr12","chr13","chr14","chr15","chr16","chr17","chr18","chr19","chr20","chr21”,”chr22”,”chrX")
-# employs the fetch method in a loop to call forth the unique and specific read mapping to a particular chromosome
+# employs the fetch method in a loop to call forth the unique and specific reads mapping to a particular chromosome
 for i in list:
     iter=samfile.fetch(i)
     print (str(i))
